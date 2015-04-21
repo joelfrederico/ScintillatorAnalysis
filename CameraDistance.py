42897a1c6988dc040f8de1089262a8bc943e1ce3
Joel Frederico <joelfred@slac.stanford.edu>
2015-04-21 16:02:51 -0700

    Formatting

--- CameraDistance.py
+++ CameraDistance.py
@@ -21,35 +21,35 @@ h_img_Ham = -13e-3
 # ====================================
 # Get magnifications
 # ====================================
-m_GigE = mag(h_img_GigE,h_obj)
-m_Ham  = mag(h_img_Ham,h_obj)
+m_GigE = mag(h_img_GigE, h_obj)
+m_Ham  = mag(h_img_Ham, h_obj)
 
 # ====================================
 # Get object distance as a
 # fxn of focal length
 # ====================================
-f_cont      = np.linspace(10,85,100)*1e-3
-o_cont_GigE = obj(f_cont,m_GigE)
-o_cont_Ham  = obj(f_cont,m_Ham)
+f_cont      = np.linspace(10, 85, 100)*1e-3
+o_cont_GigE = obj(f_cont, m_GigE)
+o_cont_Ham  = obj(f_cont, m_Ham)
 
-f_list      = np.array([20,24,28,35,50,60,85])*1e-3
-o_list_GigE = obj(f_list,m_GigE)
-o_list_Ham  = obj(f_list,m_Ham)
+f_list      = np.array([20, 24, 28, 35, 50, 60, 85])*1e-3
+o_list_GigE = obj(f_list, m_GigE)
+o_list_Ham  = obj(f_list, m_Ham)
 
 # ====================================
 # Plot results
 # ====================================
 fig = plt.figure()
-gs  = gridspec.GridSpec(1,1)
-ax  = fig.add_subplot(gs[0,0])
+gs  = gridspec.GridSpec(1, 1)
+ax  = fig.add_subplot(gs[0, 0])
 
-plt1 = ax.plot(f_cont/1e-3,o_cont_GigE,'b-',label='_GigE')
-plt2 = ax.plot(f_list/1e-3,o_list_GigE,'b-o',label='GigE')
+plt1 = ax.plot(f_cont/1e-3, o_cont_GigE, 'b-', label='_GigE')
+plt2 = ax.plot(f_list/1e-3, o_list_GigE, 'b-o', label='GigE')
 
-plt3 = ax.plot(f_cont/1e-3,o_cont_Ham,'r-',label='_Ham')
-plt4 = ax.plot(f_list/1e-3,o_list_Ham,'r-o',label='Hamamatsu')
+plt3 = ax.plot(f_cont/1e-3, o_cont_Ham, 'r-', label='_Ham')
+plt4 = ax.plot(f_list/1e-3, o_list_Ham, 'r-o', label='Hamamatsu')
 
-mt.addlabel(axes=ax,xlabel='Focal Length [mm]',ylabel='Object distance [m]',toplabel='Distance to Screen')
+mt.addlabel(axes=ax, xlabel='Focal Length [mm]', ylabel='Object distance [m]', toplabel='Distance to Screen')
 
 gs.tight_layout(fig)
 
@@ -57,13 +57,13 @@ ax.legend(loc=0)
 
 
 if __name__ == '__main__':
-    parser=argparse.ArgumentParser(description='Creates a plot of object length vs. focal length.')
-    parser.add_argument('-v','--verbose',action='store_true',
+    parser = argparse.ArgumentParser(description='Creates a plot of object length vs. focal length.')
+    parser.add_argument('-v', '--verbose', action='store_true',
             help='enable verbose mode')
-    parser.add_argument('-o','--output',action='store_true',
+    parser.add_argument('-o', '--output', action='store_true',
             help='save file')
 
-    arg=parser.parse_args()
+    arg = parser.parse_args()
 
     if arg.output:
         fig.savefig('figs/Distance_to_Screen.pdf')
